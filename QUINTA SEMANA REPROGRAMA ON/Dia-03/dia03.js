9697205448051c051106ea2bccda91a409987639
Maria Souza <messenger.mariah2017@gmail.com>
2020-04-15 17:31:50 -0300

    mais exercicios

--- QUINTA SEMANA REPROGRAMA ON/Dia-03/dia03.js
+++ QUINTA SEMANA REPROGRAMA ON/Dia-03/dia03.js
@@ -71,28 +71,97 @@
 // salario Pleno = salario base + 35%
 // salario senior = salario base + 50% 
 
-let cargo = prompt('Digite seu cargo: Jr, Pl, Sr').toLocaleLowerCase();
-let salario = '';
-const base = 2500;
+// let cargo = prompt('Digite seu cargo: Jr, Pl, Sr').toLocaleLowerCase();
+// let salario = '';
+// const base = 2500;
 
-switch (cargo) {
-    case 'junior':
-        salario = base + (base * 0.2);
-        break;
-    case 'pleno':
-        salario = base + (base * 0.35);
-        break;
-    case 'senior':
-        salario = base + (base * 0.5);
-        break;
+// switch (cargo) {
+//     case 'junior':
+//         salario = base + (base * 0.2);
+//         break;
+//     case 'pleno':
+//         salario = base + (base * 0.35);
+//         break;
+//     case 'senior':
+//         salario = base + (base * 0.5);
+//         break;
 
-    default:
-        alert('Não é um cargo base');
+//     default:
+//         alert('Não é um cargo base');
+// }
+
+// alert(`Esse é o salario ${salario}`)
+
+
+// 1. Crie um Objeto pessoa que receba as propriedades: nome, telefone, idade, altura e peso.
+
+// const pessoa = {
+//     nome: 'Maria',
+//     telefone: 11000000000,
+//     idade: 25,
+//     altura: 1.48,
+//     peso: 49
+
+// }
+//console.log(pessoa);
+
+// 2. Crie um objeto animal que receba as propriedades: porte(pequeno, médio ou grande), especie, raça.
+
+// let animal = {
+//     nome: 'Leão',
+//     porte: 'grande',
+//     especie: 'mamífero carnívoro',
+//     raça: 'leonino'
+// }
+// console.log(animal)
+
+// 3. Crie um objeto curso que receba as propriedades: nome, período, professor(a).  
+
+// let curso = {
+//     nome: 'Direito internacional',
+//     período: 'Segundo',
+//     professor: 'Lula'
+// }
+
+// curso = JSON.stringify(curso); // oJSON foi usado para converter objeto em string para que quando for dado alert aparecer na minha tela
+// alert(curso);
+
+// 1. Crie uma lista (string), que contenha o nome de 10 alunas da reprograma.
+// Em seguida, verifique o tamanho, e qual aluna está na posição 5.
+
+// let lista = ['Sara' , 'Vivi', 'Millena' , 'Tabita' , 'Liana' , 'Gabi' , 'Od' , 'Talisia' , 'Marisa' , 'Lorena'];
+
+// lista.length //ele ve o tamanho da minha lista
+// console.log(lista[4]);
+
+
+// 2. Crie uma lista de matérias que contenha 6 matérias, e mostre qual matéria está na posição 4.
+
+// let materias = ['UX', 'HTML' , 'CSS' , 'Lógica', 'Git' , 'Github'];
+// console.log(materias[4]);
+
+
+
+// 3. Crie uma lista de números fora de ordem e ordene-a utilizando o método .sort();
+
+// let numeros = [1, 2, 5, 4, 7, 6];
+
+
+// console.log(numeros.sort());
+
+// 4. Crie uma lista vazia e verifique se o length == 0 (se a lista estiver vazia) .push() inserindo algum valor dentro.
+// Se o length for maior > 0 alert() mostrando o tamanho.
+
+let lista = [];
+console.log(lista, "valor inicial da lista");
+
+if(lista.length === 0) {
+    lista.push("oi");
+    console.log(lista, "dentro do primeiro if");
+} 
+if (lista.length > 0 ) {
+    console.log(lista.length, "dento do segundo if")
 }
-
-alert(`Esse é o salario ${salario}`)
-
-
 
 
 
